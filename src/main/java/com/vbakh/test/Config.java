cfddcab9208911e626d6d07d7f65730e07f52aca
Volodymyr Bakhmatiuk <volodymyr.bakhmatiuk@rokittech.com>
2017-11-30 19:39:36 +0200

    Add specifications + projections

--- src/main/java/com/vbakh/test/Config.java
+++ src/main/java/com/vbakh/test/Config.java
@@ -4,13 +4,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import th.co.geniustree.springdata.jpa.repository.support.JpaSpecificationExecutorWithProjectionImpl;
 
 /**
  * Created by volodymyr.bakhmatiuk on 11/30/17.
  */
 @Configuration
 @EnableAutoConfiguration
-@EnableJpaRepositories
+@EnableJpaRepositories(repositoryBaseClass = JpaSpecificationExecutorWithProjectionImpl.class)
 @ComponentScan
 public class Config {
 }
--- src/main/java/com/vbakh/test/repository/DocumentRepository.java
+++ src/main/java/com/vbakh/test/repository/DocumentRepository.java
@@ -2,9 +2,10 @@ package com.vbakh.test.repository;
 
 import com.vbakh.test.domain.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import th.co.geniustree.springdata.jpa.repository.JpaSpecificationExecutorWithProjection;
 
 /**
  * Created by volodymyr.bakhmatiuk on 11/30/17.
  */
-public interface DocumentRepository extends JpaRepository<Document, Long> {
+public interface DocumentRepository extends JpaRepository<Document, Long>, JpaSpecificationExecutorWithProjection<Document> {
 }
